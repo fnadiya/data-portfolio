560f47e544c366885e0804de73064c8ea9780076
fnadiya <39280780+fnadiya@users.noreply.github.com>
2024-01-21 17:47:48 +0700

    dinner.sql

--- dinner.sql
+++ dinner.sql
@@ -76,3 +76,136 @@ from rankedProducts where rank = 1 group by customer_id, product_id;
 
 
 /*item was purchased first by the customer after they became a member*/
+select distinct order_date, product_id, customer_id from sales order by customer_id;
+
+select * from sales inner join members
+on sales.customer_id = members.customer_id;
+
+select product_id, order_date, join_date from sales inner join members
+on sales.customer_id = members.customer_id;
+
+select sales.order_date, members.join_date, sales.customer_id, sales.product_id,
+from sales inner join members
+on sales.customer_id = members.customer_id;
+
+select sales.customer_id, sales.order_date, members.join_date, sales.product_id
+from sales inner join members
+on sales.customer_id = members.customer_id
+where order_date > join_date and first;
+
+/* this one*/
+with firstPurchased as(
+ select sales.customer_id, sales.order_date, members.join_date, sales.product_id,
+	row_number() over (partition by sales.order_date order by members.join_date) as rnk
+ from sales inner join members
+ on sales.customer_id = members.customer_id
+ where sales.order_date > members.join_date
+)
+
+select customer_id, order_date, join_date, product_id
+from firstPurchased where rnk=1;
+
+---count table
+with afterPurchase as(
+	select
+		sales.customer_id,
+		sales.order_date,
+		members.join_date,
+		sales.product_id
+	from
+		sales inner join members 
+		on sales.customer_id = members.customer_id
+		left join menu on sales.product_id = menu.product_id
+	where order_date > join_date order by customer_id
+)
+select
+	customer_id,
+	count (*) as total
+from afterPurchase group by customer_id order by customer_id;
+
+---------------------------this one
+select sales.customer_id, sales.order_date, members.join_date, sales.product_id
+from sales inner join members
+on sales.customer_id = members.customer_id
+where order_date > join_date;
+
+/*item was purchased just before the customer became a member*/
+
+select sales.customer_id, sales.order_date, members.join_date, sales.product_id
+from sales inner join members
+on sales.customer_id = members.customer_id
+where sales.order_date < members.join_date;
+
+with afterPurchase as(
+	select
+		sales.customer_id,
+		sales.order_date,
+		members.join_date,
+		sales.product_id
+	from
+		sales inner join members 
+		on sales.customer_id = members.customer_id
+		left join menu on sales.product_id = menu.product_id
+	where order_date < join_date order by customer_id
+)
+select
+	customer_id,
+	count (*) as total
+from afterPurchase group by customer_id order by customer_id;
+
+
+/*the total items and amount spent for each member before they became a member*/
+select sales.customer_id, sales.order_date, members.join_date, sales.product_id
+from sales inner join members 
+on sales.customer_id = members.customer_id
+left join menu on sales.product_id = menu.product_id
+where order_date < join_date order by customer_id;
+
+------------------------this one
+with beforePurchase as(
+	select
+		sales.customer_id,
+		sales.order_date,
+		members.join_date,
+		sales.product_id
+	from
+		sales inner join members 
+		on sales.customer_id = members.customer_id
+		left join menu on sales.product_id = menu.product_id
+	where order_date < join_date order by customer_id
+)
+select
+	customer_id,
+	count (*) as total
+from beforePurchase group by customer_id order by customer_id;
+	
+
+/*If each $1 spent equates to 10 points and sushi has a 2x points multiplier
+how many points would each customer have*/
+select
+	sales.customer_id,
+	sales.product_id,
+	menu.price, 
+	count(sales.product_id*menu.price) as total
+from
+	sales left join menu
+	on sales.product_id = menu.product_id;
+	
+--------------------------------this one
+
+with totalPoints as(
+	select
+		sales.customer_id,
+		sales.product_id,
+		menu.product_name,
+		sales.product_id * menu.price as total
+	from
+		sales 
+		inner join menu
+		on sales.product_id = menu.product_id
+		order by sales.customer_id
+)
+select
+	customer_id,
+	sum(case when product_name ='sushi' then total * 20 else total * 10 end) as points
+from totalPoints group by customer_id;
