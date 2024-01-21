a533a6d307d8060d0a9ebbf3dd7b9ed2c785e7db
fnadiya <39280780+fnadiya@users.noreply.github.com>
2024-01-21 20:50:07 +0700

    dannys-dinner.sql

--- dinner.sql
+++ dinner.sql
@@ -1,211 +1,169 @@
-select * from sales;
-select * from menu;
-select * from members;
+SELECT * FROM sales;
+SELECT * FROM menu;
+SELECT * FROM members;
 
-/*count on each customer spend time*/
-select count(customer_id) as cust_spend_time
-from sales where customer_id='A';
-
-select count(customer_id) as cust_spend_time
-from sales where customer_id='B';
-
-select count(customer_id) as cust_spend_time
-from sales where customer_id='C';
----------------------------------------------
-with spendTime as(
- select customer_id,
-	count(customer_id) as cust_spend_time
- from sales group by customer_id)
- 
-select customer_id, cust_spend_time
-from spendTime order by customer_id;
-
-
-/*count on how many days each customer come to the resto*/
-select count(distinct date(order_date)) as count_days
-from sales where customer_id='A';
-
-select count(distinct date(order_date)) as count_days
-from sales where customer_id='B';
-
-select count(distinct date(order_date)) as count_days
-from sales where customer_id='C';
-------------------------------------------------------
-with spendDays as(
- select customer_id,
-	count(distinct(order_date)) as spend_days
- from sales group by customer_id)
- 
-select customer_id, spend_days
-from spendDays;
-
-
-/*the first item from the menu purchased by each customer*/
-select * from sales where customer_id='A' 
-order by order_date asc limit 1;
-
-select * from sales where customer_id='B' 
-order by order_date asc limit 1;
-
-select * from sales where customer_id='C' 
-order by order_date asc limit 1;
-------------------------------------------
-with firstItem as(
- select customer_id, product_id,
-	row_number() over(partition by customer_id order by order_date asc) as rank
- from sales)
- 
-select customer_id, product_id
-from firstItem where rank = 1;
-
-
-/*the most purchased item on the menu and how many times was it purchased by all cust*/
-select product_id, count(product_id) as most_purchased from sales
-group by product_id order by most_purchased desc limit 1;
-
-
-/*item was the most popular for each customer*/
-with rankedProducts as(
- select customer_id, product_id,
-	row_number() over(partition by customer_id order by count(*) desc) as rank
- from sales
- group by customer_id, product_id)
-
-select customer_id, product_id, count(*) as most_purchased 
-from rankedProducts where rank = 1 group by customer_id, product_id;
-
-
-/*item was purchased first by the customer after they became a member*/
-select distinct order_date, product_id, customer_id from sales order by customer_id;
-
-select * from sales inner join members
-on sales.customer_id = members.customer_id;
-
-select product_id, order_date, join_date from sales inner join members
-on sales.customer_id = members.customer_id;
-
-select sales.order_date, members.join_date, sales.customer_id, sales.product_id,
-from sales inner join members
-on sales.customer_id = members.customer_id;
-
-select sales.customer_id, sales.order_date, members.join_date, sales.product_id
-from sales inner join members
-on sales.customer_id = members.customer_id
-where order_date > join_date and first;
-
-/* this one*/
-with firstPurchased as(
- select sales.customer_id, sales.order_date, members.join_date, sales.product_id,
-	row_number() over (partition by sales.order_date order by members.join_date) as rnk
- from sales inner join members
- on sales.customer_id = members.customer_id
- where sales.order_date > members.join_date
+--count on each customer spend time
+WITH spendTime AS(
+	SELECT
+		customer_id,
+		COUNT(customer_id) AS cust_spend_time
+	FROM sales GROUP BY customer_id
 )
+SELECT
+	customer_id,
+	cust_spend_time
+FROM spendTime
+ORDER BY customer_id;
 
-select customer_id, order_date, join_date, product_id
-from firstPurchased where rnk=1;
 
----count table
-with afterPurchase as(
-	select
+--count on how many days each customer come to the resto
+WITH spendDays AS(
+	SELECT
+		customer_id,
+		COUNT(DISTINCT(order_date)) AS spend_days
+ 	FROM sales
+		GROUP BY customer_id
+) 
+SELECT
+	customer_id,
+	spend_days
+FROM spendDays;
+
+
+--the first item from the menu purchased by each customer
+WITH firstItem AS(
+	SELECT
+		customer_id,
+		product_id,
+		ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS rank
+	FROM sales
+)
+SELECT
+	customer_id,
+	product_id
+FROM firstItem
+WHERE rank = 1;
+
+
+--the most purchased item on the menu and how many times was it purchased by all cust
+SELECT
+	product_id,
+	COUNT(product_id) AS most_purchased
+FROM sales
+GROUP BY product_id
+ORDER BY most_purchased DESC LIMIT 1;
+
+
+--item was the most popular for each customer
+WITH rankedProducts AS(
+	SELECT 
+		customer_id,
+		product_id,
+		ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY COUNT(*) DESC) AS rank
+	FROM sales
+ 	GROUP BY customer_id, product_id
+)
+SELECT
+	customer_id,
+	product_id,
+	count(*) as most_purchased 
+FROM rankedProducts
+WHERE rank = 1
+GROUP BY customer_id, product_id;
+
+
+-- Item was purchased first by the customer after they became a member
+--show all data
+WITH firstPurchased AS(
+	SELECT
+		sales.customer_id,
+		sales.order_date,
+		members.join_date,
+		sales.product_id,
+		ROW_NUMBER() OVER (PARTITION BY sales.order_date ORDER BY members.join_date)AS rnk
+	FROM sales
+	INNER JOIN members
+ 	ON sales.customer_id = members.customer_id
+	WHERE sales.order_date > members.join_date
+)
+SELECT
+	customer_id, order_date, join_date, product_id
+FROM firstPurchased WHERE rnk=1;
+
+--counted result
+WITH afterPurchase AS(
+	SELECT
 		sales.customer_id,
 		sales.order_date,
 		members.join_date,
 		sales.product_id
-	from
-		sales inner join members 
-		on sales.customer_id = members.customer_id
-		left join menu on sales.product_id = menu.product_id
-	where order_date > join_date order by customer_id
+	FROM
+		sales INNER JOIN members 
+		ON sales.customer_id = members.customer_id
+		LEFT JOIN menu ON sales.product_id = menu.product_id
+	WHERE order_date > join_date ORDER BY customer_id
 )
-select
+SELECT
 	customer_id,
-	count (*) as total
-from afterPurchase group by customer_id order by customer_id;
+	COUNT (*) AS total
+FROM afterPurchase GROUP BY customer_id ORDER BY customer_id;
 
----------------------------this one
-select sales.customer_id, sales.order_date, members.join_date, sales.product_id
-from sales inner join members
-on sales.customer_id = members.customer_id
-where order_date > join_date;
 
-/*item was purchased just before the customer became a member*/
-
-select sales.customer_id, sales.order_date, members.join_date, sales.product_id
-from sales inner join members
-on sales.customer_id = members.customer_id
-where sales.order_date < members.join_date;
-
-with afterPurchase as(
-	select
+--item was purchased just before the customer became a member*/
+WITH afterPurchase AS(
+	SELECT
 		sales.customer_id,
 		sales.order_date,
 		members.join_date,
 		sales.product_id
-	from
-		sales inner join members 
-		on sales.customer_id = members.customer_id
-		left join menu on sales.product_id = menu.product_id
-	where order_date < join_date order by customer_id
+	FROM
+		sales INNER JOIN members 
+		ON sales.customer_id = members.customer_id
+		LEFT JOIN menu ON sales.product_id = menu.product_id
+	WHERE order_date < join_date ORDER BY customer_id
 )
-select
+SELECT
 	customer_id,
-	count (*) as total
-from afterPurchase group by customer_id order by customer_id;
+	COUNT (*) AS total
+FROM afterPurchase
+GROUP BY customer_id
+ORDER BY customer_id;
 
 
-/*the total items and amount spent for each member before they became a member*/
-select sales.customer_id, sales.order_date, members.join_date, sales.product_id
-from sales inner join members 
-on sales.customer_id = members.customer_id
-left join menu on sales.product_id = menu.product_id
-where order_date < join_date order by customer_id;
-
-------------------------this one
-with beforePurchase as(
-	select
+--the total items and amount spent for each member before they became a member*/
+WITH beforePurchase AS(
+	SELECT
 		sales.customer_id,
 		sales.order_date,
 		members.join_date,
 		sales.product_id
-	from
-		sales inner join members 
-		on sales.customer_id = members.customer_id
-		left join menu on sales.product_id = menu.product_id
-	where order_date < join_date order by customer_id
+	FROM
+		sales INNER JOIN members 
+		ON sales.customer_id = members.customer_id
+		LEFT JOIN menu ON sales.product_id = menu.product_id
+	WHERE order_date < join_date ORDER BY customer_id
 )
-select
+SELECT
 	customer_id,
-	count (*) as total
-from beforePurchase group by customer_id order by customer_id;
+	COUNT (*) AS total
+FROM beforePurchase GROUP BY customer_id ORDER BY customer_id;
 	
 
-/*If each $1 spent equates to 10 points and sushi has a 2x points multiplier
-how many points would each customer have*/
-select
-	sales.customer_id,
-	sales.product_id,
-	menu.price, 
-	count(sales.product_id*menu.price) as total
-from
-	sales left join menu
-	on sales.product_id = menu.product_id;
-	
---------------------------------this one
-
-with totalPoints as(
-	select
+--If each $1 spent equates to 10 points and sushi has a 2x points multiplier how many points would each customer have
+WITH totalPoints AS(
+	SELECT
 		sales.customer_id,
 		sales.product_id,
 		menu.product_name,
 		sales.product_id * menu.price as total
-	from
+	FROM
 		sales 
-		inner join menu
-		on sales.product_id = menu.product_id
-		order by sales.customer_id
+		INNER JOIN menu
+		ON sales.product_id = menu.product_id
+		ORDER BY sales.customer_id
 )
-select
+SELECT
 	customer_id,
-	sum(case when product_name ='sushi' then total * 20 else total * 10 end) as points
-from totalPoints group by customer_id;
+	SUM(CASE WHEN product_name ='sushi' THEN total * 20 ELSE total * 10 END) AS points
+FROM totalPoints
+GROUP BY customer_id;
